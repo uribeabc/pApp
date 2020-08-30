ef5eb6da056946d6dc181064a4d69866b5a3afcc
uribeabc <uribeabc@gmail.com>
2020-08-30 17:55:24 -0400

    Initial Commit

---
+++ API/pApp.API/Data/DataContext.cs
@@ -0,0 +1,14 @@
+using Microsoft.EntityFrameworkCore;
+using pApp.API.Models;
+
+namespace pApp.API.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Value> Values { get; set; }
+    }
+}
